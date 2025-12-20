02cfdf6d43bb9a34345516cdc762ba8306782454
Benoit Travers <benoit.travers.fr@gmail.com>
2025-12-20 01:14:58 +0000

    feat: logo

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -4,18 +4,24 @@ export default defineConfig({
   title: "Benoit TRAVERS",
   description: "Personal website",
   base: '/',
+
   themeConfig: {
     logo: '/logo.svg',
+
     nav: [
       { text: 'Home', link: '/' }
     ],
+    
     socialLinks: [
       { icon: 'github', link: 'https://github.com/btravers' },
       { icon: 'linkedin', link: 'https://linkedin.com/in/btraversfr' }
     ],
+    
     footer: {
       message: 'Built with VitePress',
       copyright: `Copyright © ${new Date().getFullYear()} btravers`
     }
-  }
+  },
+
+  head: [["link", { rel: "icon", type: "image/svg+xml", href: "/logo.svg" }]],
 })
